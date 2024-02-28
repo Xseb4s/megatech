0393f58bab8b75a7bf9dd83d1838fcecf0c6de7f
Xseb4s <sebas25qr@gmail.com>
2024-02-28 15:46:33 -0500

    fix: fixed an error to build

--- src/components/whatsapp/index.jsx
+++ src/components/whatsapp/index.jsx
@@ -1,15 +1,22 @@
-import WhatsApp from "/megatech/icons/whatsapp.svg"
+
+
 const Whatsapp = () => {
   return (
-    <a href="https://api.whatsapp.com/message/7EJ3JJN4UHDJC1" rel="noreferrer" target="_blank" aria-label="Comunicarse al Whatsapp" className="max-sm:hidden lg:fixed z-50 bottom-5 right-10 flex flex-col items-center text-green-700"> 
+    <a
+      href="https://api.whatsapp.com/message/7EJ3JJN4UHDJC1"
+      rel="noreferrer"
+      target="_blank"
+      aria-label="Comunicarse al Whatsapp"
+      className="max-sm:hidden lg:fixed z-50 bottom-5 right-10 flex flex-col items-center text-green-700"
+    >
       <figure>
-        <img src={WhatsApp} alt="WhatsApp logo" className="w-full h-full"/>
+        <img src="/megatech/icons/whatsapp.svg" alt="WhatsApp logo" className="w-full h-full" />
         <figcaption className="border border-green-700 bg-white px-2 rounded-3xl">
           Escribenos
-        </figcaption>  
+        </figcaption>
       </figure>
     </a>
-  )
-}
+  );
+};
 
-export default Whatsapp
+export default Whatsapp;
